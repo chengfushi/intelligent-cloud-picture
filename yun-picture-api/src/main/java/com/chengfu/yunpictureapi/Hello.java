54d86a94c850f4e9f4525a88d3fc5337e09f7f04
perfect <164840332+camellialoveit@users.noreply.github.com>
2025-07-17 18:51:13 +0800

    Delete yun-picture-api/src/main/java/com/chengfu/yunpictureapi/Hello.java

--- yun-picture-api/src/main/java/com/chengfu/yunpictureapi/Hello.java
+++
@@ -1,7 +0,0 @@
-package com.chengfu.yunpictureapi;
-
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello World from feature/print branch!");
-    }
-}
